51ac0a3cfbaaccbc41f53d1d7864c66cf2e90e84
Isaias <99995191+isaias30silva@users.noreply.github.com>
2022-05-20 17:04:33 -0300

    exercicios16 e 17 concluídos

---
+++ Aulas/aula14.ex/ex016/script.js
@@ -0,0 +1,31 @@
+function contar() {
+    var ini = document.getElementById('txtI')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+        } else {
+            //Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
